88ab42a8f2c65dd46cd8fea9d70cb0161a1b67b0
Aggelos Biboudis <biboudis@gmail.com>
2015-07-26 17:13:22 +0300

    Remove elpa.

--- .emacs.d/elpa/popup-20150626.711/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("popup.el") (21940 59802 111496 229000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
